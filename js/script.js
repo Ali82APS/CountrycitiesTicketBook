5d3075c551d1dbf32c9c779c609721e7ee546f5f
Ali82Aps <a.82aps@gmail.com>
2024-02-23 23:41:47 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+﻿let countrySelectBox = document.querySelector(".countrySelect");
+let citySelect = document.querySelector(".citySelect");
+
+let countriesData = {
+  Iran:[Tehran,Kerman,Shiraz],
+  Turkey:[Ezmir,Ankara],
+  US:[Seattle,Newyork,chicago,mami]
+};
+
+countrySelectBox.addEventListener("change", function () {
+
+  if (countrySelectBox.value === "Please Select") {
+    citySelect.innerHTML = "";
+    citySelect.innerHTML += "<option>Select City</option>";
+  } else {
+    let mainCountryName = countrySelectBox.value; // Us
+    let mainCountryCities = countriesData[mainCountryName];
+
+    citySelect.innerHTML = "";
+
+    mainCountryCities.forEach(function (city) {
+      citySelect.innerHTML += "<option>" + city + "</option>";
+    });
+  }
+});
+
+
+
+//---------------Ali82Aps------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
